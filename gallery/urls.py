c5e24f301cf4974fdf406ce56af4e0b7a0b61abc
Melby01 <melby.okozi@student.moringaschool.com>
2021-07-01 17:54:59 +0300

    Update all changes done

--- gallery/urls.py
+++ gallery/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
     url('^today/$',views.image_of_day,name='imageToday'),
-    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_image,name = 'pastImage') 
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_image,name = 'pastImage') 
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -1,4 +1,4 @@
-from django.http  import HttpResponse
+from django.http  import HttpResponse,Http404
 import datetime as dt
 
 # Create your views here.
@@ -30,11 +30,18 @@ def convert_dates(dates):
     day = days[day_number]
     return day
 
-def past_days_image(request,past_date):
+def past_image(request,past_date):
+    try:
+        # Converts data from the string Url
+     date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
         # Converts data from the string Url
     date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
-
     day = convert_dates(date)
+    
     html = f'''
         <html>
             <body>
