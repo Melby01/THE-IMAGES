cc6bd63585aede8a832ab19b12dceac08bacd749
Melby01 <melby.okozi@student.moringaschool.com>
2021-07-01 17:42:14 +0300

    Getting day of the week

--- gallery/urls.py
+++ gallery/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
-     url('^today/$',views.image_of_day,name='newsToday')
+     url('^today/$',views.image_of_day,name='imageToday')
 ]
