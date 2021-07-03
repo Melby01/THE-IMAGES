af9c805a4c75e3ef41fbeb5b7bf8b4f92739cd83
Melby01 <melby.okozi@student.moringaschool.com>
2021-07-04 01:51:47 +0300

    Updating a nwe view function

--- gallery/urls.py
+++ gallery/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
-    url('^today/$',views.image_of_day,name='imageToday'),
+    url('^$',views.image_of_day,name='imageToday'),
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_image,name = 'pastImage')
 ]
